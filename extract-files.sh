c59c7f99a154c636cceb7ead55ca5806c473ed32
Griffin Millender <griffinn.millender@gmail.com>
2013-12-10 04:16:00 -0600

    Update props
    Change-Id: I606c3f4e7eeaef559eedf525dd65a56469e9fba3

--- extract-files.sh
+++ extract-files.sh
@@ -1,17 +1,4 @@
 #!/bin/sh
 
-VENDOR=lge
-DEVICE=d800
-
-BASE=../../../vendor/$VENDOR/$DEVICE/proprietary
-rm -rf $BASE/*
-
-for FILE in `cat proprietary-blobs.txt | grep -v ^# | grep -v ^$ `; do
-    DIR=`dirname $FILE`
-    if [ ! -d $BASE/$DIR ]; then
-        mkdir -p $BASE/$DIR
-    fi
-    cp $1/$FILE $BASE/$FILE
-done
-
-./setup-makefiles.sh
+# This script is maintained in the common tree
+../msm8660-common/extract-files.sh $*
