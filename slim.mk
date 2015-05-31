fc81524726e94447e5ace81149c6dc206ab730f9
Griffin Millender <griffinn.millender@gmail.com>
2015-05-31 04:21:27 -0500

    Changes for slim
    Change-Id: I1e2c2fc1104da7f3b72170740afb0b4ddab53dc1

--- slim.mk
+++ slim.mk
@@ -3,17 +3,17 @@ TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/slim/config/common_full_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/lge/d800/d800.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := d800
-PRODUCT_NAME := cm_d800
+PRODUCT_NAME := slim_d800
 PRODUCT_BRAND := LGE
 PRODUCT_MODEL := LG-D800
 PRODUCT_MANUFACTURER := lge
